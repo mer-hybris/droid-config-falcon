5233e0f09841a44d73d58a3ebbc70205d7ff1e06
Adam Pigg <adam@piggz.co.uk>
2016-03-26 17:19:19 +0000

    Reduce time surfaceflinger runs

--- sparse/usr/bin/droid/lipstick-hack.sh
+++ sparse/usr/bin/droid/lipstick-hack.sh
@@ -3,5 +3,5 @@
 # surfaceflinger needs to run for a bit, in order for sailfish graphics to work
 # very weird, proper solution needed
 /system/bin/surfaceflinger &
-sleep 5
+sleep 3
 killall surfaceflinger
