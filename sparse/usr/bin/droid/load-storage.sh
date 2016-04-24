82cf961a809fd8001ece646a5bbbe932df5c921b
Adam Pigg <adam@piggz.co.uk>
2016-04-24 22:13:58 +0100

    New storage service

--- sparse/usr/bin/droid/load-storage.sh
+++ sparse/usr/bin/droid/load-storage.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-
-if getent group media_rw | grep &>/dev/null '\nemo\b'; then
-    echo true
-else
-    usermod -a -G media_rw nemo
-fi
-
-
-rm -rf /home/nemo/Music
-rm -rf /home/nemo/Pictures
-rm -rf /home/nemo/.vault
-rm -rf /home/nemo/storage
-
+#Fixup permissions
 chmod 0777 /data
 chmod 0777 /data/media
 chmod 0777 /data/media/0
@@ -20,10 +8,11 @@ chmod 0777 /data/media/0/DCIM
 chmod -R 0777 /data/media/0/Music
 chmod -R 0777 /data/media/0/DCIM/Camera
 
-ln -sfn /data/media/0/Music /home/nemo/Music
-ln -sfn /data/media/0/DCIM/Camera /home/nemo/Pictures
+#Create folder to store vault
+mkdir -p  /data/media/0/storage
+
+#Create link to sdcard
+ln -sfn /data/media/0 /home/nemo/android_storage
 ln -sfn /data/media/0/storage /home/nemo/.vault
 
-#chown -R nemo /home/nemo/Pictures
-#chown -R nemo /home/nemo/.vault
-#chown -R nemo /home/nemo/Music
+chown -R nemo /home/nemo/.vault
--- sparse/usr/bin/droid/unload-storage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#rm /home/nemo/Music
-unlink /home/nemo/Music
-unlink /home/nemo/Pictures
-unlink /home/nemo/.vault
